f8497f3d07d344ec0f9a7b6a4e8d322d0dc7ba61
유지수 Jisoo Yoo <youzysu@gmail.com>
2023-08-27 21:25:12 +0900

    Revert "[Team-05-FE] 1주차 PR"

--- src/test/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "App";
-
-test("Renders main element", () => {
-  render(<App />);
-  const mainElement = screen.getByRole("main");
-  expect(mainElement).toBeInTheDocument();
-});

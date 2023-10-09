e62934434864fcd09d2be6df876a0b80433060f7
pie <yejinh_h@naver.com>
2023-10-09 23:32:25 +0900

    #1 docs : 프로젝트 초기 설정

---
+++ src/main/java/pie/tomato/TomatoMarketApplication.java
@@ -0,0 +1,13 @@
+package pie.tomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatoMarketApplication.class, args);
+	}
+
+}

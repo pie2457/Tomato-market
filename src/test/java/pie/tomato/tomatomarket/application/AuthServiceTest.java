0cb0da72417f8c93b3333b8a4cf62a3a3e7d7053
pie <yejinh_h@naver.com>
2023-10-17 18:34:36 +0900

    test #6 : 회원가입 테스트 작성

---
+++ src/test/java/pie/tomato/tomatomarket/application/AuthServiceTest.java
@@ -0,0 +1,61 @@
+package pie.tomato.tomatomarket.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import pie.tomato.tomatomarket.application.oauth.KakaoClient;
+import pie.tomato.tomatomarket.domain.Member;
+import pie.tomato.tomatomarket.exception.BadRequestException;
+import pie.tomato.tomatomarket.exception.ErrorCode;
+import pie.tomato.tomatomarket.infrastructure.persistence.MemberRepository;
+
+@SpringBootTest
+@Transactional
+class AuthServiceTest {
+
+	@Autowired
+	private AuthService authService;
+	@Autowired
+	private MemberRepository memberRepository;
+	@MockBean
+	private KakaoClient kakaoClient;
+
+	@DisplayName("회원가입에 성공한다.")
+	@Test
+	void signup() {
+		// given
+		accessTokenAndUserInfo(new Member("pie123", "pie-choco@pie.com", "pie/image.jpg"));
+
+		// when
+		authService.signup("code");
+
+		// then
+		assertThat(memberRepository.existsMemberByEmail("pie-choco@pie.com")).isTrue();
+	}
+
+	@DisplayName("중복된 email로 회원가입 요청 시 실패한다.")
+	@Test
+	void DuplicateEmailSignupFail() {
+		// given
+		Member saveMember = memberRepository.save(new Member("pie123", "pie-choco@pie.com", "pie/image.jpg"));
+		accessTokenAndUserInfo(saveMember);
+
+		// when & then
+		assertThatThrownBy(
+			() -> authService.signup("code")).isInstanceOf(BadRequestException.class)
+			.extracting("errorCode").isEqualTo(ErrorCode.ALREADY_EXIST_MEMBER);
+	}
+
+	void accessTokenAndUserInfo(Member member) {
+		given(kakaoClient.getAccessToken(anyString())).willReturn("abc.abc.abc");
+		given(kakaoClient.getUserInfo(anyString())).
+			willReturn(member);
+	}
+}

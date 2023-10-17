7bd19aee8f67bbd43cd65d93f7f89a4dcd0f5700
pie <yejinh_h@naver.com>
2023-10-18 00:25:20 +0900

    refactor #6 : 리뷰사항 반영
    생성자를 애노테이션으로 변경 및 개행

--- src/main/java/pie/tomato/tomatomarket/exception/ErrorResponse.java
+++ src/main/java/pie/tomato/tomatomarket/exception/ErrorResponse.java
@@ -1,14 +1,12 @@
 package pie.tomato.tomatomarket.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class ErrorResponse {
+
 	private final int statusCode;
 	private final String message;
-
-	public ErrorResponse(int statusCode, String message) {
-		this.statusCode = statusCode;
-		this.message = message;
-	}
 }

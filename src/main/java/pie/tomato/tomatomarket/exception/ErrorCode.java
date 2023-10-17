07322eb9fd372898b943508eb6440fe784925c19
pie <yejinh_h@naver.com>
2023-10-17 18:45:37 +0900

    fest #6 : 에러 코드 작성
    로그인 시 회원가입된 멤버가 아니라면 NOT_FOUND 에러코드를 반환한다.

--- src/main/java/pie/tomato/tomatomarket/exception/ErrorCode.java
+++ src/main/java/pie/tomato/tomatomarket/exception/ErrorCode.java
@@ -15,7 +15,8 @@ public enum ErrorCode {
 	EXPIRED_TOKEN(HttpStatus.FORBIDDEN, "토큰이 만료되었습니다."),
 
 	// Member
-	ALREADY_EXIST_MEMBER(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다.");
+	ALREADY_EXIST_MEMBER(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
+	NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "회원을 찾지 못하였습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;

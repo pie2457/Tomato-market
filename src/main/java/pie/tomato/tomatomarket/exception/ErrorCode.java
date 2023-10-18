0aae0411021bbad54c832185f2a49192ad844665
pie <yejinh_h@naver.com>
2023-10-18 14:46:39 +0900

    feat #8 : 에러코드 생성 및 exception 추가

--- src/main/java/pie/tomato/tomatomarket/exception/ErrorCode.java
+++ src/main/java/pie/tomato/tomatomarket/exception/ErrorCode.java
@@ -16,7 +16,8 @@ public enum ErrorCode {
 
 	// Member
 	ALREADY_EXIST_MEMBER(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
-	NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "회원을 찾지 못하였습니다.");
+	NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "회원을 찾지 못하였습니다."),
+	ALREADY_EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;

178965c25c0f8c5309bfa7aacd85682f32dc4edf
pie <yejinh_h@naver.com>
2023-10-18 14:52:26 +0900

    fix #8 : @Getter 애노테이션 추가
    에러코드 406 / HttpMediaTypeNotAcceptableException 발생 하여 Getter를 추가해 문제 해결

--- src/main/java/pie/tomato/tomatomarket/presentation/response/LoginResponse.java
+++ src/main/java/pie/tomato/tomatomarket/presentation/response/LoginResponse.java
@@ -1,10 +1,11 @@
-package pie.tomato.tomatomarket.application.oauth.response;
+package pie.tomato.tomatomarket.presentation.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class LoginResponse {
 
 	private String accessToken;
-
 }

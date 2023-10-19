8907f7d6ccc6ff86c3930a38661d6d60befb56be
pie <yejinh_h@naver.com>
2023-10-19 16:04:38 +0900

    docs #10 : S3Properties 작성
    s3에 사진을 올리기 위해 필요한 환경변수를 .yml파일에서 읽어오도록 설정.

---
+++ src/main/java/pie/tomato/tomatomarket/infrastructure/config/properties/S3Properties.java
@@ -0,0 +1,39 @@
+package pie.tomato.tomatomarket.infrastructure.config.properties;
+
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@ConfigurationProperties("cloud.aws")
+public class S3Properties {
+
+	private final Credentials credentials;
+	private final S3 s3;
+	private final String region;
+
+	@ConstructorBinding
+	public S3Properties(Credentials credentials, S3 s3, Map<String, String> region) {
+		this.credentials = credentials;
+		this.s3 = s3;
+		this.region = region.get("static");
+	}
+
+	@Getter
+	@RequiredArgsConstructor
+	public static class Credentials {
+
+		private final String accessKey;
+		private final String secretKey;
+	}
+
+	@Getter
+	@RequiredArgsConstructor
+	public static class S3 {
+		private final String bucket;
+	}
+}

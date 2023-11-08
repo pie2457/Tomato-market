19838d0c1492ac77d2a9758f59640e9e5270712d
pie <yejinh_h@naver.com>
2023-11-08 12:00:41 +0900

    feat #34 : 관심상품의 카테고리 조회

---
+++ src/main/java/pie/tomato/tomatomarket/presentation/response/wish/CategoryWishListResponse.java
@@ -0,0 +1,10 @@
+package pie.tomato.tomatomarket.presentation.response.wish;
+
+import lombok.Getter;
+
+@Getter
+public class CategoryWishListResponse {
+
+	private Long categoryId;
+	private String categoryName;
+}

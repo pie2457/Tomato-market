f257e259af817d90598026ba06dde9e48777f458
pie <yejinh_h@naver.com>
2023-12-07 15:57:21 +0900

    refactor #68 : 채팅방 목록을 불러올 때 데이터를 잘못가져와 코드 수정
    - ChatLogRepository : chatLog 테이블에서 chatroomId에 해당하는 가장 마지막 메세지와 메세지의 시간을 가져옴
    - ChatPaginationRepository : queryDsl로 response 데이터를 한번에 처리하지 않고 필요한 정보만 가져오게 함.
    - ChatroomListResponse : queryDsl로 못가져온 데이터를 가져다가 채워넣어줌
    - ChatroomService : response안에 들어갈 데이터들을 찾아오게끔 수정

--- src/main/java/pie/tomato/tomatomarket/application/chat/ChatroomService.java
+++ src/main/java/pie/tomato/tomatomarket/application/chat/ChatroomService.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import pie.tomato.tomatomarket.domain.ChatLog;
 import pie.tomato.tomatomarket.domain.Chatroom;
 import pie.tomato.tomatomarket.domain.Item;
 import pie.tomato.tomatomarket.domain.Member;
 import pie.tomato.tomatomarket.exception.ErrorCode;
 import pie.tomato.tomatomarket.exception.NotFoundException;
+import pie.tomato.tomatomarket.infrastructure.persistence.chat.ChatLogRepository;
 import pie.tomato.tomatomarket.infrastructure.persistence.chat.ChatPaginationRepository;
 import pie.tomato.tomatomarket.infrastructure.persistence.chat.ChatroomRepository;
 import pie.tomato.tomatomarket.infrastructure.persistence.item.ItemRepository;
@@ -28,6 +30,7 @@ public class ChatroomService {
 
 	private final ChatPaginationRepository chatPaginationRepository;
 	private final ChatroomRepository chatroomRepository;
+	private final ChatLogRepository chatLogRepository;
 	private final MemberRepository memberRepository;
 	private final ItemRepository itemRepository;
 
@@ -38,13 +41,28 @@ public class ChatroomService {
 		List<ChatroomListResponse> content = responses.getContent();
 
 		for (ChatroomListResponse chatroomListResponse : content) {
-			Long messageCount = chatroomRepository.getNewMessageCount(chatroomListResponse.getChatroomId());
-			chatroomListResponse.assignNewMessageCount(messageCount);
+			assignResponseInfo(principal, chatroomListResponse);
 		}
 
 		Long nextCursor = content.isEmpty() ? null : content.get(content.size() - 1).getChatroomId();
 
 		return new CustomSlice<>(content, nextCursor, responses.hasNext());
+	}
+
+	private void assignResponseInfo(Principal principal, ChatroomListResponse chatroomListResponse) {
+		ChatLog findChatLog = chatLogRepository.findFirstByChatroomId(chatroomListResponse.getChatroomId());
+		chatroomListResponse.assignLastMessageInfo(findChatLog);
+
+		Long messageCount = chatroomRepository.getNewMessageCount(chatroomListResponse.getChatroomId());
+		chatroomListResponse.assignNewMessageCount(messageCount);
+
+		Chatroom chatroom = findChatroom(chatroomListResponse.getChatroomId());
+		chatroomListResponse.assignPartnerInfo(chatroom, principal);
+	}
+
+	private Chatroom findChatroom(Long chatroomId) {
+		return chatroomRepository.findById(chatroomId)
+			.orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_CHATROOM));
 	}
 
 	@Transactional
--- src/main/java/pie/tomato/tomatomarket/presentation/chat/response/ChatroomListResponse.java
+++ src/main/java/pie/tomato/tomatomarket/presentation/chat/response/ChatroomListResponse.java
@@ -3,6 +3,9 @@ package pie.tomato.tomatomarket.presentation.chat.response;
 import java.time.LocalDateTime;
 
 import lombok.Getter;
+import pie.tomato.tomatomarket.domain.ChatLog;
+import pie.tomato.tomatomarket.domain.Chatroom;
+import pie.tomato.tomatomarket.presentation.support.Principal;
 
 @Getter
 public class ChatroomListResponse {
@@ -17,5 +20,20 @@ public class ChatroomListResponse {
 
 	public void assignNewMessageCount(Long newMessageCount) {
 		this.newMessageCount = newMessageCount;
+	}
+
+	public void assignLastMessageInfo(ChatLog chatLog) {
+		this.lastSendTime = chatLog.getCreatedAt();
+		this.lastSendMessage = chatLog.getMessage();
+	}
+
+	public void assignPartnerInfo(Chatroom chatroom, Principal principal) {
+		if (chatroom.getSeller().getId() == principal.getMemberId()) {
+			this.chatPartnerName = chatroom.getBuyer().getNickname();
+			this.chatPartnerProfile = chatroom.getBuyer().getProfile();
+		} else {
+			this.chatPartnerName = chatroom.getSeller().getNickname();
+			this.chatPartnerProfile = chatroom.getSeller().getProfile();
+		}
 	}
 }

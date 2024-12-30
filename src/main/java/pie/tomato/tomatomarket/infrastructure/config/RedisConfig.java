d822755578456bd99d9c714d3903a4c49372756d
pie <104147789+pie2457@users.noreply.github.com>
2024-12-30 18:28:54 +0900

    feat #85 : redis sentinel 설정 (#86)

--- src/main/java/pie/tomato/tomatomarket/infrastructure/config/RedisConfig.java
+++ src/main/java/pie/tomato/tomatomarket/infrastructure/config/RedisConfig.java
@@ -1,9 +1,13 @@
 package pie.tomato.tomatomarket.infrastructure.config;
 
+import java.util.stream.Collectors;
+
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -14,19 +18,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RedisConfig {
 
-	private final RedisProperties redisProperties;
+    private final RedisProperties redisProperties;
 
-	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
-	}
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        var nodes = redisProperties.getSentinel().getNodes().stream().map(node -> {
+            String[] hostAndPort = node.split(":");
+            return new RedisNode(hostAndPort[0], Integer.parseInt(hostAndPort[1]));
+        }).collect(Collectors.toList());
+        var sentinelConfiguration = new RedisSentinelConfiguration()
+            .master(redisProperties.getSentinel().getMaster());
+        sentinelConfiguration.setSentinels(nodes);
 
-	@Bean
-	public RedisTemplate<String, String> redisTemplate() {
-		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
-		redisTemplate.setConnectionFactory(redisConnectionFactory());
-		return redisTemplate;
-	}
+        return new LettuceConnectionFactory(sentinelConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
 }
